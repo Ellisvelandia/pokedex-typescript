9456b5107e58ab8cd1afc80a1dc0d295f4947032
Ellisvelandia <eyis619l@gmail.com>
2022-12-19 20:38:31 -0500

    pokedex

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home, PokeDetail } from "./pages";
+
+import "./index.scss";
+import PokemonProvider from "./context/PokemonContext";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/:pokeId",
+    element: <PokeDetail />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <PokemonProvider>
+    <RouterProvider router={router} />
+  </PokemonProvider>
+);
